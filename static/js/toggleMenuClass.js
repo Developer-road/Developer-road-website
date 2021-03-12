ef07ba39dcdd82d0e05246607fb9d639a3697538
Nicolas-alt <a10100105555@gmail.com>
2021-03-11 19:49:52 -0500

    Fix bugs console

--- static/js/toggleMenuClass.js
+++ static/js/toggleMenuClass.js
@@ -1,13 +1,20 @@
-// Menú profile options
-const menu = document.getElementById('navbarDropdownMenuLink');
-const dropDown = document.getElementById('ulDropDown');
-menu.addEventListener('click', () =>
-  dropDown.classList.toggle('ul_dropDown-visible')
-);
-
 // Menú links
 const menuLinks = document.getElementById('menuLinks');
 const menuIcons = document.getElementById('iconDropDown');
 menuIcons.addEventListener('click', () =>
   menuLinks.classList.toggle('ul_dropDownLinks-visible')
 );
+
+// Close search bar
+const closeSearchBar = document.getElementById('i_search');
+closeSearchBar.addEventListener('click', () => console.log('f'));
+
+// Menú profile options
+const menu = document.getElementById('navbarDropdownMenuLink');
+
+if (menu) {
+  const dropDown = document.getElementById('ulDropDown');
+  menu.addEventListener('click', () =>
+    dropDown.classList.toggle('ul_dropDown-visible')
+  );
+}
