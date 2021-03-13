d4f3ed8a1bb45eba3685ad8d50cd25263df0422e
Daniel Diaz <danieldiazgonza52@gmail.com>
2021-03-12 22:27:57 -0500

    Added liked urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import (BlogView, ArticleDetail, PostCreateView, EditPost,
-                    PostDeleteView, CategoryCreateView, CategoryUpdateView, CategoryView, CategoryListView, VoteView, BlogSearchView)
+                    PostDeleteView, CategoryCreateView, CategoryUpdateView,
+                     CategoryView, CategoryListView, VoteView, BlogSearchView,
+                     BlogMostLikedView)
 
 app_name = 'blog'
 urlpatterns = [
     path('', BlogView.as_view(), name="blog_page"),
+    path('liked/', BlogMostLikedView.as_view(), name="blog_liked_page"),
     path('article/<int:pk>', ArticleDetail.as_view(), name="article_page"),
     path('article/edit/<int:pk>', EditPost.as_view(), name="edit_page"),
     path('article/<int:pk>/delete', PostDeleteView.as_view(), name="delete_page"),
