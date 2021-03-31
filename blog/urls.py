9aa75c49a1b781a53891303c539ecddc37c46d97
Daniel Diaz <danieldiazgonza52@gmail.com>
2021-03-30 23:37:52 -0500

    {Modified} modified url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,11 @@
+"""
+Blog Urls
+"""
+
+# Django
 from django.urls import path
+
+# Views
 from .views import (BlogView,
                     ArticleDetail,
                     PostCreateView,
@@ -10,68 +17,68 @@ from .views import (BlogView,
                     CategoryListView,
                     VoteView,
                     BlogSearchView,
-                    BlogMostLikedView)
+                    )
 
 app_name = 'blog'
 
 
 urlpatterns = [
     # Default blog home page
+    # Main url, accepts filter=liked
     path('',
          BlogView.as_view(),
-         name="blog_page"),
+         name="home"),
 
-    # Filtered most like blog page
-    path('liked/',
-         BlogMostLikedView.as_view(),
-         name="blog_liked_page"),
+    path('search/',
+         BlogSearchView.as_view(),
+         name="search"),
 
     # Detail url of a blog post
     # parameter: pk of a blog post
     path('article/<int:pk>/',
          ArticleDetail.as_view(),
-         name="article_page"),
+         name="article"),
 
     # Edit page of a blog post,
     # parameter: primary key of a blog post
     path('article/<int:pk>/edit/',
          EditPost.as_view(),
-         name="edit_page"),
+         name="edit_article"),
 
     # Delete page of a blog post,
     # only the creator can delete the post
     # parameter: pk of the blog post
     path('article/<int:pk>/delete/',
          PostDeleteView.as_view(),
-         name="delete_page"),
+         name="delete_article"),
 
     # Create page for a blog post
     path('create/',
          PostCreateView.as_view(),
-         name="add_post"),
+         name="create_article"),
 
     # Create category page,
     # only logged users can create one
     path('create_category/',
          CategoryCreateView.as_view(),
-         name="add_category"),
+         name="create_category"),
 
     # List all the categories
     path('categories/',
          CategoryListView.as_view(),
-         name="categories_page"),
+         name="categories"),
 
     # Detail category view
     # parameter: the category name
     path('category/<str:cat>/',
          CategoryView.as_view(),
-         name="category_page"),
+         name="category"),
 
     # Category edit page
     # parameter: pk of category
     path('category/<int:pk>/edit/',
          CategoryUpdateView.as_view(),
-         name="category_edit"),
+         name="edit_category"),
 
     # Upvotes category handler
     # Function view that adds an upvote to the blog post
@@ -79,9 +86,4 @@ urlpatterns = [
     path('upvotes/<int:pk>/',
          VoteView,
          name="upvotes"),
-
-    # Search stuff
-    path('search/',
-         BlogSearchView.as_view(),
-         name="search"),
 ]
