8560b8366f660f473695e9b27a23462a3783358d
Daniel Diaz <danieldiazgonza52@gmail.com>
2021-03-30 23:36:43 -0500

    {Added} differentiation between Edit and Create forms

--- blog/forms.py
+++ blog/forms.py
@@ -4,39 +4,42 @@ from django_bleach.forms import BleachField
 from .models import Post, Category, Comment
 
 
-class PostForm(forms.ModelForm):
+class BaseCRUDPostForm(forms.ModelForm):
     class Meta:
         """
         Returns the form of  the add post page
         """
         model = Post
+
         fields = ('title', 'description', 'header_image', 'body', 'category',)
+
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', "placeholder": "An awesome title 🤓"}),
-            'description': forms.Textarea(attrs={'class': 'form-control', "placeholder": "Describe your Post briefly"}),
+            'title': forms.TextInput(attrs={'class': 'form-control',
+                                            "placeholder": "An awesome title 🤓"}),
+            'description': forms.Textarea(attrs={'class': 'form-control',
+                                                 "placeholder": "Describe your Post briefly"}),
             'body': BleachField(),
+
             'category': forms.Select(attrs={'class': 'form-control'}),
-            'header_image': forms.FileInput(attrs={'class': 'form-control'}),
-        }
+
+            'header_image': forms.FileInput(attrs={'class': 'form-control'}), }
 
 
-class EditPostForm(forms.ModelForm):
-    class Meta:
-        """
-        Returns the form of  the add post page
-        """
-        model = Post
-        fields = ('title', 'description', 'header_image', 'body', 'category',)
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': BleachField(),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-            'header_image': forms.FileInput(attrs={'class': 'form-control'}),
-        }
+class PostForm(BaseCRUDPostForm):
+    """
+    Form used to create a Post
+    """
+
+class EditPostForm(BaseCRUDPostForm):
+    """
+    Form used to edit a Post
+    """
 
 
-class CreateCategoryForm(forms.ModelForm):
+class BaseCRUDCategoryForm(forms.ModelForm):
+    """
+    Base CRUD operations for categories model
+    """
     class Meta:
         """
         Returns the form of the add category page
@@ -48,6 +51,15 @@ class CreateCategoryForm(forms.ModelForm):
             'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
+class EditCategoryForm(BaseCRUDCategoryForm):
+    """
+    Edit category form for Category edit page
+    Only available for admins
+    """
+
+
+class CreateCategoryForm(BaseCRUDCategoryForm):
+
     def clean_name(self):
 
         data = self.cleaned_data.get('name')
@@ -55,7 +67,6 @@ class CreateCategoryForm(forms.ModelForm):
         if Category.objects.filter(name__iexact=data).count() >= 1:
             raise forms.ValidationError('Sorry that category already exist')
         return data
-
 
 
 class CommentForm(forms.ModelForm):
@@ -73,4 +84,3 @@ class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CommentForm, self).__init__(*args, **kwargs)
         self.fields["body"].required = True
-
